21f82a47bcad5b2b0533a63b7dc0a2fd1a86df48
SohamRatnaparkhi <soham.ratnaparkhi@gmail.com>
2023-03-07 20:39:18 +0530

    select queries added

--- customer/Select_customer.sql
+++ customer/Select_customer.sql
@@ -3,3 +3,23 @@ SELECT * FROM customer
     ON customer.customer_id = addresses.address_id
     JOIN names
     ON customer.customer_id = names.name_id;
+
+SELECT DISTINCT city FROM addresses;
+
+SELECT COUNT(address_id), city 
+    FROM addresses
+    GROUP BY city;
+
+
+ SELECT * FROM addresses
+    WHERE city = 'Pune' OR city = 'Mumbai';
+
+
+SELECT * FROM addresses
+    JOIN customer
+    ON addresses.address_id = customer.customer_id;
+
+SELECT * FROM addresses
+    -> INNER JOIN customer
+    -> ON addresses.address_id = customer.customer_id;
+
