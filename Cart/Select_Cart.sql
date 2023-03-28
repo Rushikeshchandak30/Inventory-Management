d76b69296d98f699cf5111616dee6b7c5cec280a
TejasRokade <111081649+Tejas3103@users.noreply.github.com>
2023-03-28 23:06:56 +0530

    Added more select Quries...
    The last 6 new Select Queries were added....

--- Cart/Select_Cart.sql
+++ Cart/Select_Cart.sql
@@ -22,4 +22,20 @@ SELECT * FROM customer
 SELECT * FROM customer
     CROSS JOIN Cart
     ON customer.customer_id = Cart.customerid;
+    
+    
+SELECT *from cart order by customerid;
 
+SELECT DISTINCT ProductName from Cart;
+
+SELECT sum(TotalPrice) from cart;
+
+SELECT * FROM CART WHERE TotalPrice='5000' or TotalPrice= '500';
+
+SELECT customerid,ProductName
+     From Cart
+     WHERE TotalPrice is not NULL;
+     
+ SELECT CURRENT_TIMESTAMP AS CurrentServerDateTime;
+ 
+ SELECT ASCII('A');
